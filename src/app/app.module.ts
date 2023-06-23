4cb3088c936f8453b5370853bbad5ce630e794d9
ionchiriac99 <i.chiriac1999@gmail.com>
2023-06-23 14:17:34 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, CoreModule],
+	imports: [BrowserModule, BrowserAnimationsModule, CoreModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
